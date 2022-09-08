e0d824f4a22f286387718f7aa7102347644d0f27
Cube X <shawn6304@qq.com>
2022-09-08 15:33:14 +0800

    学习通助手优化

--- 学习通网课助手.js
+++ 学习通网课助手.js
@@ -10,20 +10,38 @@
 // ==/UserScript==
 
 (function() {
+    //自动下一节
+    if(document.URL.includes('mycourse/studentstudy?')){
+        window.onload=()=>{
+            var lis=document.getElementsByClassName('catalog_points_yi prevTips');
+            var c=lis.length;
+            for(var i=0;i<lis.length;i++){ //获取第一个未完成任务点
+                if(lis[i].innerText=='2'){ c=i-1; break;}
+            }
+            setInterval(()=>{ //自动向下
+                if(c<lis.length&&lis[c].innerText=='1'){
+                    c++;
+                    lis[c].parentElement.children[0].click();
+                }
+            },1000);
+        }
+    }
+
+    //控制播放
     if(document.URL.includes('ananas/modules/video/index.html?v=')){
         var v;
-        var begin=10,n=0;
+        var last=0,n=0;
         var stop=setInterval(()=>{
             v=document.getElementById('video_html5_api');
-            if(v){
-                //播放
-                //v.play();
-                //拉取进度条
-                var time=document.getElementsByClassName('vjs-duration-display')[0].innerHTML.split(':');
-                if(time[0]!='0'){ begin=time[0]*60+parseInt(time[1])-30; }
+            //拉取进度条
+            var time=document.getElementsByClassName('vjs-duration-display')[0].innerHTML.split(':');
+            if(time[0]!='0'){
+                last=time[0]*60+parseInt(time[1]);
+                //v.currentTime=last-10;
             }
             //静音
-            var vol=document.getElementsByClassName('vjs-mute-control vjs-control vjs-button vjs-vol-3')[0]; if(vol) vol.click();
+            //var vol=document.getElementsByClassName('vjs-mute-control vjs-control vjs-button vjs-vol-3')[0]; if(vol) vol.click();
+            v.volume=0;
             //暂停
             //v.pause = null;
             //解锁进度条 index
@@ -35,29 +53,39 @@
             seekbar.prototype.handleMouseUp = function(c){
                 seekbar.prototype.__proto__.handleMouseUp .call(this,c);}
             videojs("video").off("seeked");
+            //解锁鼠标
+            Ext.apply=null;
+            //解锁倍数
+            videojs("video").playbackRate=null;
+            //播放
+            v.play();
 
-            if(!v.pause&&v&&seekbar){
+            if(last&&v&&seekbar){
                 clearInterval(stop);
                 window.v=v;
             }
         },2000);
-
-        /*setInterval(()=>{
-            if(begin==v.currentTime) v.play();
-            v.playbackRate=16; //倍数
-            if(n%500==0) v.currentTime+=5; //进度条
-            n++;
-        },1) //*/
+        //倍数控制
+        setInterval(()=>{
+            if(last&&last-10<=v.currentTime){
+                v.playbackRate=1;
+            }else{ v.playbackRate=8;}
+        },1000) //*/
     }
 
+    //模拟阅读
     if(document.URL.includes('ztnodedetailcontroller')){
         var li=document.getElementsByClassName('wh wh');
         window.onblur=null;
         setInterval(()=>{
-            var a=li[parseInt(Math.random()*li.length)];
-            if(a.tagName=='A' && Math.random()<0.1) a.click(); //0.1的概率跳转
-            window.scrollTo(0,Math.random()*10000); //每10s滚动一次
-        },1000*10)
+            n++;
+            if(0==n%parseInt(Math.random()*60+60)){ //每1-2min滚动一次
+                window.onfocus();
+                var a=li[parseInt(Math.random()*li.length)];
+                if(a.tagName=='A' && Math.random()<0.1) a.click();
+                window.scrollTo(0,Math.random()*10000);
+            }
+        },1000);
     }
 
 })();
