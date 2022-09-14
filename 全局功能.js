db531346027d68e4a57d1462cd7d3815b410b576
Cube X <shawn6304@qq.com>
2022-09-14 22:23:17 +0800

    全局功能 新增选择复制功能

--- 全局功能.js
+++ 全局功能.js
@@ -5,17 +5,20 @@
 // @description  设计模式开关 广告隐藏
 // @author       Cubxx
 // @include    *
-// @exclude  /chaoxing/
+// @exclude   file:///*
 // @icon         data:image/svg+xml,%3C?xml version='1.0' encoding='utf-8'?%3E%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 400 400'%3E%3Cg id='XMLID_273_'%3E%3Cg id='XMLID_78_'%3E%3Cpath id='XMLID_83_' class='st0' d='M304.8,0H95.2C42.6,0,0,42.6,0,95.2v209.6C0,357.4,42.6,400,95.2,400h209.6 c52.6,0,95.2-42.6,95.2-95.2V95.2C400,42.6,357.4,0,304.8,0z M106.3,375C61.4,375,25,338.6,25,293.8c0-44.9,36.4-81.3,81.3-81.3 c44.9,0,81.3,36.4,81.3,81.3C187.5,338.6,151.1,375,106.3,375z M293.8,375c-44.9,0-81.3-36.4-81.3-81.3 c0-44.9,36.4-81.3,81.3-81.3c44.9,0,81.3,36.4,81.3,81.3C375,338.6,338.6,375,293.8,375z'/%3E%3C/g%3E%3Cg id='XMLID_67_' class='st2'%3E%3Cpath id='XMLID_74_' class='st3' d='M304.8,0H95.2C42.6,0,0,42.6,0,95.2v209.6C0,357.4,42.6,400,95.2,400h209.6 c52.6,0,95.2-42.6,95.2-95.2V95.2C400,42.6,357.4,0,304.8,0z M106.3,375C61.4,375,25,338.6,25,293.8c0-44.9,36.4-81.3,81.3-81.3 c44.9,0,81.3,36.4,81.3,81.3C187.5,338.6,151.1,375,106.3,375z M293.8,375c-44.9,0-81.3-36.4-81.3-81.3 c0-44.9,36.4-81.3,81.3-81.3c44.9,0,81.3,36.4,81.3,81.3C375,338.6,338.6,375,293.8,375z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E
 // @grant        none
 // ==/UserScript==
 
 (function(){
+    var global=document.createElement('div');
+    global.id='全局功能组';
+
     //设计模式
-    var 设计开关=true;
     (function() {
+        var 设计开关=true;
         var btn=document.createElement('input');
-        btn.id='设计模式_开关';
+        btn.id='设计模式';
         btn.type='button';
         btn.onclick=function(){
             if(设计开关){
@@ -42,7 +45,7 @@ font: bold 17px/20px caption;';
         btn.style.opacity=0.1;
         btn.value='设计模式-关';
         setInterval(function(){
-            if(document.getElementById('设计模式_开关')===null){ document.body.appendChild(btn); }
+            if(!document.body.contains(btn)){ global.appendChild(btn); }
         },1000);
     })();
 
@@ -72,4 +75,25 @@ font: bold 17px/20px caption;';
         },100);
     })();
 
+    //邮箱发送
+    /*(function(){
+        var btn=document.createElement('input');
+        btn.id='邮箱发送';
+        btn.type='button';
+        btn.onclick=function(){
+            var str=window.getSelection().toString();
+            if(str){} //loading...//
+        }
+        setInterval(function(){
+            if(document.body.contains(btn)){ global.appendChild(btn); }
+        },1000);
+    })();
+*/
+
+    //选择复制
+    document.onkeydown=function(e){ //Ctrl+C
+        if(e.ctrlKey && e.keyCode===67 && window.getSelection().toString()) document.execCommand('copy');
+    }
+
+    document.body.appendChild(global);
 })();
