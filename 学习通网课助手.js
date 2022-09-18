346f9428f99d891ac81fda29b5b7617394f16838
Cube X <shawn6304@qq.com>
2022-09-18 21:53:44 +0800

    刷完网课暂停

--- 学习通网课助手.js
+++ 学习通网课助手.js
@@ -2,94 +2,100 @@
 // @name         学习通网课助手
 // @namespace    https://github.com/cubxx
 // @version      0.1
-// @description  try to take over the world!
+// @description  I'm lazy dog
 // @author       Cubxx
 // @match      https://mooc1.chaoxing.com/*
 // @icon         https://mooc.chaoxing.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     //自动下一节
-    if(document.URL.includes('mycourse/studentstudy?')){
-        window.onload=()=>{
-            var lis=document.getElementsByClassName('posCatalog_select'); //右侧菜单所有元素
-            var c=lis.length;
-            for(var i=0;i<lis.length;i++){
-                if(lis[i].childElementCount==3 && lis[i].children[1].value=='1'){ c=i; break;} //获取第一个未完成任务点
+    if (document.URL.includes('mycourse/studentstudy?')) {
+        window.onload = () => {
+            let lis = document.getElementsByClassName('posCatalog_select'); //右侧菜单所有元素
+            let c = lis.length;
+            for (let i = 0; i < lis.length; i++) {
+                if (lis[i].childElementCount == 3 && lis[i].children[1].value == '1') { c = i; break; } //获取第一个未完成任务点
                 //if(lis[i].className.includes('posCatalog_active')){ c=i; break;} //获取当前选中任务点
             }
             lis[c].children[0].click();
-            setInterval(()=>{ //自动向下
-                var duration=parseInt(sessionStorage.getItem('dur'));
-                var time=parseInt(sessionStorage.getItem('time'));
-                if((duration && time>=duration) || lis[c].childElementCount!=3){
+            let stop = setInterval(() => { //自动向下
+                let duration = parseInt(sessionStorage.getItem('dur'));
+                let time = parseInt(sessionStorage.getItem('time'));
+                if ((duration && time >= duration) || lis[c].childElementCount != 3) {
                     //if(c<lis.length && lis[c].childElementCount!=3){
                     c++;
-                    if(lis[c].childElementCount==1) c++;
+                    (lis[c].childElementCount == 1) && c++;
                     lis[c].children[0].click();
-                    sessionStorage.setItem('time',0);
+                    sessionStorage.setItem('time', 0);
                 }
-            },5*1000); //每5s检查一次
+                document.getElementById('iframe').contentDocument
+                    .getElementsByTagName('iframe')[0].contentDocument
+                    .getElementsByClassName('liveCard')[0] && clearInterval(stop);
+            }, 5 * 1000); //每5s检查一次
         }
     }
 
     //控制播放
-    if(document.URL.includes('ananas/modules/video/index.html?v=')){
-        window.onerror=()=>{return true} //隐藏报错
-        var v;
-        var duration=0,n=0;
-        var stop=setInterval(()=>{
-            v=document.getElementById('video_html5_api');
+    if (document.URL.includes('ananas/modules/video/index.html?v=')) {
+        window.onerror = () => { return true } //隐藏报错
+        let v;
+        let duration = 0, n = 0;
+        let stop = setInterval(() => {
+            v = document.getElementById('video_html5_api');
             //播放
             v.play();
             //拉取进度条
-            duration=v.duration;
-            sessionStorage.setItem('dur',duration);
+            duration = v.duration;
+            sessionStorage.setItem('dur', duration);
             //v.currentTime=duration-10;
             //静音
-            v.volume=0;
+            v.volume = 0;
             //解锁进度条
             let seekbar = videojs.getComponent("SeekBar");
-            seekbar.prototype.handleMouseDown = function(c){
-                seekbar.prototype.__proto__.handleMouseDown.call(this,c);}
-            seekbar.prototype.handleMouseMove = function(c){
-                seekbar.prototype.__proto__.handleMouseMove.call(this,c);}
-            seekbar.prototype.handleMouseUp = function(c){
-                seekbar.prototype.__proto__.handleMouseUp .call(this,c);}
+            seekbar.prototype.handleMouseDown = function (c) {
+                seekbar.prototype.__proto__.handleMouseDown.call(this, c);
+            }
+            seekbar.prototype.handleMouseMove = function (c) {
+                seekbar.prototype.__proto__.handleMouseMove.call(this, c);
+            }
+            seekbar.prototype.handleMouseUp = function (c) {
+                seekbar.prototype.__proto__.handleMouseUp.call(this, c);
+            }
             videojs("video").off("seeked");
             //解锁鼠标
-            Ext.apply=null;
+            Ext.apply = null;
             //解锁倍数
-            videojs("video").playbackRate=function(){};
+            videojs("video").playbackRate = function () { };
 
-            if(v && seekbar && !isNaN(duration)){
+            if (v && seekbar && !isNaN(duration)) {
                 clearInterval(stop);
-                window.v=v;
+                window.v = v;
             }
-        },2000);
+        }, 2000);
         //倍数控制
-        setInterval(()=>{
-            sessionStorage.setItem('time',v.currentTime);
+        setInterval(() => {
+            sessionStorage.setItem('time', v.currentTime);
             /*if(duration&&duration-10<=v.currentTime){
                 v.playbackRate=1;
             }else{ v.playbackRate=4;}*/
-        },5*1000)
+        }, 5 * 1000)
     } //*/
 
     //模拟阅读
-    if(document.URL.includes('ztnodedetailcontroller')){
-        var li=document.getElementsByClassName('wh wh');
-        window.onblur=null;
-        setInterval(()=>{
+    if (document.URL.includes('ztnodedetailcontroller')) {
+        let li = document.getElementsByClassName('wh wh'), n = 0;
+        window.onblur = null;
+        setInterval(() => {
             n++;
-            if(0==n%parseInt(Math.random()*60+60)){ //每1-2min滚动一次
+            if (0 == n % parseInt(Math.random() * 60 + 60)) { //每1-2min滚动一次
                 window.onfocus();
-                var a=li[parseInt(Math.random()*li.length)];
-                if(a.tagName=='A' && Math.random()<0.1) a.click();
-                window.scrollTo(0,Math.random()*10000);
+                let a = li[parseInt(Math.random() * li.length)];
+                if (a.tagName == 'A' && Math.random() < 0.1) a.click();
+                window.scrollTo(0, Math.random() * 10000);
             }
-        },1000);
+        }, 1000);
     }
 
 })();
