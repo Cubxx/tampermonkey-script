2378336e27802fde682c1def38086bcb4cd73861
Cube X <shawn6304@qq.com>
2022-09-21 17:23:12 +0800

    新增 邮箱发送

--- 全局功能.js
+++ 全局功能.js
@@ -6,58 +6,58 @@
 // @author       Cubxx
 // @include    *
 // @exclude   file:///*
+// @exclude   https://cubxx.github.io/*
 // @icon         data:image/svg+xml,%3C?xml version='1.0' encoding='utf-8'?%3E%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 400 400'%3E%3Cg id='XMLID_273_'%3E%3Cg id='XMLID_78_'%3E%3Cpath id='XMLID_83_' class='st0' d='M304.8,0H95.2C42.6,0,0,42.6,0,95.2v209.6C0,357.4,42.6,400,95.2,400h209.6 c52.6,0,95.2-42.6,95.2-95.2V95.2C400,42.6,357.4,0,304.8,0z M106.3,375C61.4,375,25,338.6,25,293.8c0-44.9,36.4-81.3,81.3-81.3 c44.9,0,81.3,36.4,81.3,81.3C187.5,338.6,151.1,375,106.3,375z M293.8,375c-44.9,0-81.3-36.4-81.3-81.3 c0-44.9,36.4-81.3,81.3-81.3c44.9,0,81.3,36.4,81.3,81.3C375,338.6,338.6,375,293.8,375z'/%3E%3C/g%3E%3Cg id='XMLID_67_' class='st2'%3E%3Cpath id='XMLID_74_' class='st3' d='M304.8,0H95.2C42.6,0,0,42.6,0,95.2v209.6C0,357.4,42.6,400,95.2,400h209.6 c52.6,0,95.2-42.6,95.2-95.2V95.2C400,42.6,357.4,0,304.8,0z M106.3,375C61.4,375,25,338.6,25,293.8c0-44.9,36.4-81.3,81.3-81.3 c44.9,0,81.3,36.4,81.3,81.3C187.5,338.6,151.1,375,106.3,375z M293.8,375c-44.9,0-81.3-36.4-81.3-81.3 c0-44.9,36.4-81.3,81.3-81.3c44.9,0,81.3,36.4,81.3,81.3C375,338.6,338.6,375,293.8,375z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E
 // @grant        none
 // ==/UserScript==
 
 (function () {
-    var global = document.createElement('div');
-    global.id = '全局功能组';
-
-    { //设计模式
-        let 设计开关 = true;
-        let btn = document.createElement('input');
-        btn.id = '设计模式';
-        btn.type = 'button';
-        btn.onclick = function () {
-            if (设计开关) {
-                document.designMode = 'on';
-                btn.value = '设计模式-开';
-                btn.style.color = '#fff';
-                btn.style.backgroundColor = '#000';
-            } else {
-                document.designMode = 'off';
-                btn.value = '设计模式-关';
-                btn.style.color = '#000';
-                btn.style.backgroundColor = '#fff0';
-            }
-            设计开关 = !设计开关;
-        };
-        btn.style = 'position:fixed;z-index:9999;border:1px solid #000;width:120px;height:40px;color:#000;background-color:#fff0;font:bold 17px/20px caption;';
-        btn.style.top = '0px', btn.style.left = '-110px';
-        btn.value = '设计模式-关';
-        setInterval(function () {
-            if (!document.body.contains(btn)) { global.appendChild(btn); }
-        }, 1000);
-        let _mmove = document.onmousemove || function () { }
-        btn.onmousedown = function (e) {
+    let _mmove = document.onmousemove || function () { };
+    let elm = {
+        style: 'border:none;width:auto;height:35px;color:#000;background-color:#fff0;font:bold 17px/20px caption;',
+        onmousedown: function (e) {
+            let _this = this;
             var ex = e.clientX, ey = e.clientY,
-                px = parseFloat(window.getComputedStyle(btn).left),
-                py = parseFloat(window.getComputedStyle(btn).top);
+                px = parseFloat(window.getComputedStyle(this).left),
+                py = parseFloat(window.getComputedStyle(this).top);
             var dx = px - ex, dy = py - ey;
             document.onmousemove = function (e) {
                 var ex = e.clientX, ey = e.clientY;
-                btn.style.left = dx + ex + 'px';
-                btn.style.top = dy + ey + 'px';
-                设计开关 = false;
+                _this.style.left = dx + ex + 'px';
+                _this.style.top = dy + ey + 'px';
                 _mmove(e);
             }
-            btn.onmouseup = function () {
+            this.onmouseup = function () {
                 document.onmousemove = _mmove;
-                if (parseFloat(btn.style.left) < 0) btn.style.left = '-110px';
-                if (parseFloat(btn.style.top) < 0) btn.style.top = '0px';
+                if (parseFloat(this.style.left) < 0) this.style.left = '-90px';
+                if (parseFloat(this.style.top) < 0) this.style.top = '0px';
+            }
+        },
+        onclick: function (obj, on_off, func1, func2 = () => { }) {
+            if (on_off) func1(), obj.style.backgroundColor = '#fff0';
+            else func2(), obj.style.backgroundColor = '#0003';
+        }
+    }
+    var global = document.createElement('div');
+    global.id = '全局功能组';
+    global.style = 'display:flex;flex-direction:column;position:fixed;top:0;left:-90px;z-index:9999;width:100px;border:3px solid #000;border-radius:10px';
+    global.onmousedown = elm.onmousedown;
+
+    { //设计模式
+        let 设计开关 = false;
+        let btn = document.createElement('input');
+        btn.id = '设计模式';
+        btn.type = 'button';
+        btn.value = '设计模式';
+        btn.style = elm.style;
+        btn.onmousedown = function () {
+            let ismove = false;
+            btn.onmousemove = function () { ismove = true }
+            btn.onmouseup = function () {
+                !ismove && (elm.onclick(this, 设计开关, () => { document.designMode = 'off' }, () => { document.designMode = 'on' }), 设计开关 = !设计开关)
             }
         }
+        setInterval(function () { !document.body.contains(btn) && global.appendChild(btn) }, 1000);
     }
 
     { //广告删除
@@ -86,18 +86,25 @@
         }, 100);
     }
 
-    /*{ //邮箱发送
-        var btn=document.createElement('input');
-        btn.id='邮箱发送';
-        btn.type='button';
-        btn.onclick=function(){
-            var str=window.getSelection().toString();
-            if(str){} //loading...//
+    { //邮箱发送
+        let btn = document.createElement('input');
+        btn.id = '邮箱发送';
+        btn.type = 'button';
+        btn.value = '邮箱发送';
+        btn.style = elm.style;
+        btn.onmousedown = function () {
+            let ismove = false;
+            btn.onmousemove = function () { ismove = true }
+            btn.onmouseup = function () {
+                !ismove && elm.onclick(this, 1, () => {
+                    let str = window.getSelection().toString();
+                    if (str && /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(str)) window.open('mailto:' + str);
+                    else alert('邮箱格式错误');
+                })
+            }
         }
-        setInterval(function(){
-            if(document.body.contains(btn)){ global.appendChild(btn); }
-        },1000);
-    }    */
+        setInterval(function () { !document.body.contains(btn) && global.appendChild(btn) }, 1000);
+    }
 
     //选择复制
     let _onkeydown = document.onkeydown || function () { };
